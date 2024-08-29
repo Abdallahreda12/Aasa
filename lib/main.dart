9257ea2b5ae96cebb99e9d023347b958d3a63123
Abdallah Reda <abdallahreda664@gmail.com>
2024-08-29 13:15:44 +0300

    remove "Flutter"

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demoooooo',
+      title: 'Demoooooo',
       theme: ThemeData(
         // This is the theme of your application.
         //
